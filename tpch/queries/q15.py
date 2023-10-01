590d65e3791b8ed9dae4a1fa33cbc9896d014b9f
csruiliu <rui.liu@proton.me>
2023-10-01 19:01:03 -0400

    update queries

--- tpch/queries/q15.py
+++ tpch/queries/q15.py
@@ -1,23 +1,29 @@
-query = ["""
-CREATE TEMP VIEW REVENUE (SUPPLIER_NO, TOTAL_REVENUE) AS
-SELECT	L_SUPPKEY,
-        sum(L_EXTENDEDPRICE * (1 - L_DISCOUNT))
-FROM    lineitem
-WHERE   L_SHIPDATE >= '1996-01-01'
-        AND L_SHIPDATE < '1996-04-01'
-GROUP BY    L_SUPPKEY
-""", """
+query = """
 SELECT	S_SUPPKEY,
 		S_NAME,
 		S_ADDRESS,
 		S_PHONE,
 		TOTAL_REVENUE
 FROM	supplier,
-        REVENUE
+        (       
+            SELECT	L_SUPPKEY as SUPPLIER_NO,
+                    sum(L_EXTENDEDPRICE * (1 - L_DISCOUNT)) as TOTAL_REVENUE,
+            FROM    lineitem
+            WHERE   L_SHIPDATE >= '1996-01-01'
+                    AND L_SHIPDATE < '1996-04-01'
+            GROUP BY    L_SUPPKEY
+        )
 WHERE	S_SUPPKEY = SUPPLIER_NO
 		AND TOTAL_REVENUE = (
 			SELECT	MAX(TOTAL_REVENUE)
-			FROM	REVENUE
+			FROM	(
+			            SELECT	L_SUPPKEY as SUPPLIER_NO,
+                        sum(L_EXTENDEDPRICE * (1 - L_DISCOUNT)) as TOTAL_REVENUE,
+                        FROM    lineitem
+                        WHERE   L_SHIPDATE >= '1996-01-01'
+                                AND L_SHIPDATE < '1996-04-01'
+                        GROUP BY    L_SUPPKEY
+			        )   
 		)
 ORDER BY	S_SUPPKEY
-"""]
+"""
