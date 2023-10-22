3035e0673f9423e6a339877de47fd2297e4cf1c6
csruiliu <rui.liu@proton.me>
2023-10-21 22:32:50 -0400

    update riveter

--- riveter.py
+++ riveter.py
@@ -1,134 +1,120 @@
 import argparse
+import subprocess
+import ctypes
+import sysv_ipc
+import json
+import yaml
+import toml
 import time
 import pandas as pd
-import subprocess
-import psutil
-import sysv_ipc
-import ctypes
 import numpy as np
-import matplotlib.pyplot as plt
 
-from scipy.optimize import curve_fit
+from pathlib import Path
 
-# Constants 
+from cost_model import ProcLatencyEstimator, PipelineLatencyEstimator
+
+# Constants
 RAND_WRITE_SPEED = 2500
 RAND_READ_SPEED = 2500
 TERM_PROB = 0.7
 
 
-class ProcLatencyEstimator:
-    def __init__(self,
-                 rand_write_speed,
-                 rand_read_speed,
-                 num_join_array,
-                 num_groupby_array,
-                 input_cardinality_array,
-                 suspension_point_array,
-                 persistence_size_array):
-        # rand r/w speed of hardware storage
-        self.rand_write_speed = rand_write_speed
-        self.rand_read_speed = rand_read_speed
-
-        # the data for regression
-        self.num_join_array = num_join_array
-        self.num_groupby_array = num_groupby_array
-        self.input_cardinality_array = input_cardinality_array
-        self.suspension_point_array = suspension_point_array
-
-        # the results/label for regression
-        self.persistence_size_array = persistence_size_array
-
-        # the param estimated from regression
-        self.param = None
-
+class PropertyUtils:
     @staticmethod
-    def func_persistence_size(para, a, b, c, d, e):
-        result = 1 / (a * para[0] + b * para[1] + c * para[2] + d * para[3] + e)
-        return result.ravel()
+    def load_property_file(properties_file):
+        if isinstance(properties_file, str):
+            properties_file = Path(properties_file)
 
-    def fit_curve(self):
-        assert len(self.num_join_array) == len(self.input_cardinality_array) == len(self.suspension_point_array)
-        x = np.column_stack(self.num_join_array, self.num_groupby_array, self.input_cardinality_array,
-                            self.suspension_point_array)
-        y = self.persistence_size_array
-        self.param, _ = curve_fit(self.func_persistence_size, x, y)
+        with open(properties_file) as fp:
+            if properties_file.suffix == ".json":
+                properties = json.load(fp)
 
-    def persist_size_estimation(self, num_join, num_groupby, input_card, suspension_point):
-        return (num_join * self.param[0] + num_groupby * self.param[1] + input_card * self.param[2]
-                + suspension_point * self.param[2])
+            elif properties_file.suffix == ".yaml":
+                properties = yaml.load(fp, Loader=yaml.SafeLoader)
 
-    def suspend_latency_estimation(self, num_join, num_groupby, input_card, suspension_point):
-        return self.persist_size_estimation(num_join, num_groupby, input_card, suspension_point) / self.rand_write_speed
+            elif properties_file.suffix == ".toml":
+                properties = toml.load(fp)
+            else:
+                raise ValueError(f'Error loading {properties_file.name}. {properties_file.suffix} is not supported')
 
-    def resume_latency_estimation(self, num_join, num_groupby, input_card, suspension_point):
-        return self.persist_size_estimation(num_join, num_groupby, input_card, suspension_point) / self.rand_read_speed
+        return properties
 
 
-class PipelineLatencyEstimator:
-    def __init__(self, persistence_size, rand_write_speed, rand_read_speed):
-        self.persistence_size = persistence_size
-        self.rand_write_speed = rand_write_speed
-        self.rand_read_speed = rand_read_speed
-
-    def suspend_latency_estimation(self):
-        return self.persistence_size / self.rand_write_speed
-
-    def resume_latency_estimation(self):
-        return self.persistence_size / self.rand_read_speed
-
-
-def profile_hardware():
-    hw_threads_num = psutil.cpu_count(logical=True)
-    total_memory_gb = psutil.virtual_memory()[0] / 1000000000
-    available_memory_gb = psutil.virtual_memory()[1] / 1000000000
-
-    print(f"Number of Hardware Threads: {hw_threads_num}")
-    print(f"Total Memory Size (GB): {total_memory_gb}")
-    print(f"Available Memory Size (GB): {available_memory_gb}")
-
-    return hw_threads_num, total_memory_gb, available_memory_gb
-
-
-def assemble_execution_cmd():
+def demo_proc_latency_estimation():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-ef", "--estimation_file", type=str, action="store", required=True,
+                        help="indicate the file stored historical data for estimation")
+    args = parser.parse_args()
+    estimation_file = args.estimation_file
 
+    start = time.perf_counter()
+
+    # Load json file for estimation
+    query_json = PropertyUtils.load_property_file(properties_file=estimation_file)
+    query_executions = query_json["query_executions"]
+
+    num_join_list = list()
+    num_groupby_list = list()
+    input_card_list = list()
+    suspension_point_list = list()
+    persist_size_list = list()
+
+    for qe in query_executions:
+        num_join_list.append(qe["num_join"])
+        num_groupby_list.append(qe["num_groupby"])
+        input_card_list.append(qe["input_cardinality"])
+        suspension_point_list.append(qe["suspension_point"])
+        persist_size_list.append(qe["persistence_size"])
+
+    ple = ProcLatencyEstimator(RAND_WRITE_SPEED, RAND_READ_SPEED,
+                               num_join_list, num_groupby_list,
+                               input_card_list, suspension_point_list, persist_size_list)
+    ple.fit_curve()
+
+    input_num_join = 1
+    input_num_groupby = 2
+    input_cardinality = 75100030
+    input_suspension_point = 0.5
+
+    persisted_data_size = ple.persist_size_estimation(input_num_join,
+                                                      input_num_groupby,
+                                                      input_cardinality,
+                                                      input_suspension_point)
+
+    print("Estimated Persisted Data Size: {}".format(persisted_data_size))
+    end = time.perf_counter()
+    print("Total Runtime: {}".format(end - start))
+
+
+def demo_e2e():
+    parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--benchmark", type=str, action="store", required=True,
                         choices=["vanilla", "tpch", "tpcds"],
                         help="indicate the benchmark for evaluation")
-
     parser.add_argument("-q", "--query_id", type=str, action="store", required=True,
                         help="indicate the query id")
-
     parser.add_argument("-d", "--database", type=str, action="store", default="memory",
                         help="indicate the database location, memory or other location")
-
     parser.add_argument("-df", "--data_folder", type=str, action="store", required=True,
                         help="indicate the TPC-H dataset for Vanilla Queries, such as <exp/dataset/tpch/parquet-sf1>")
-
     parser.add_argument("-tmp", "--tmp_folder", type=str, action="store", default="tmp",
                         help="indicate the tmp folder for DuckDB, such as <exp/tmp>")
-
     parser.add_argument("-sl", "--suspend_location", type=str, action="store",
                         help="indicate the file or folder for suspending query")
-
     parser.add_argument("-ts", "--termination_start", type=int, action="store",
                         help="indicate the starting point of termination time window")
-
     parser.add_argument("-te", "--termination_end", type=int, action="store",
                         help="indicate the starting point of termination time window")
-
     parser.add_argument("-tu", "--time_unit", type=int, action="store",
                         help="indicate the time unit for moving forward when estimating latency for proc-level")
-
     parser.add_argument("-nj", "--number_join", type=int, action="store",
                         help="indicate the number of join operator in a query plan")
-
     parser.add_argument("-ng", "--number_groupby", type=int, action="store",
                         help="indicate the cardinality of input dataset")
-
     parser.add_argument("-ic", "--input_cardinality", type=int, action="store",
                         help="indicate the cardinality of input dataset")
-
+    parser.add_argument("-ef", "--estimation_file", type=str, action="store", required=True,
+                        help="indicate the file stored historical data for estimation")
     args = parser.parse_args()
 
     benchmark = args.benchmark
@@ -141,6 +127,7 @@ def assemble_execution_cmd():
     num_join = args.number_join
     num_groupby = args.number_groupby
     input_card = args.input_cardinality
+    estimation_file = args.estimation_file
 
     benchmark_arg = f"{benchmark}/ratchet_{benchmark}.py"
     if args.database is None:
@@ -148,12 +135,14 @@ def assemble_execution_cmd():
     else:
         db_arg = f"{benchmark}/{args.database}.db"
 
+    # Get start time point
+    e2e_start_time = time.perf_counter()
+
     ratchet_cmd = f"python3 {benchmark_arg} -q {qid} -d {db_arg} -df {data_folder} -s -sl {sloc}"
 
-    return ratchet_cmd, ts, te, time_step, num_join, num_groupby, input_card
+    # Execute the query through subprocess
+    ratchet_proc = subprocess.Popen([ratchet_cmd], shell=True)
 
-
-def get_shm_variable():
     # Match the keyfile with the C++ program
     shm_cost_model_flag_keyfile = "/tmp/shm_cost_model_flag_keyfile"
     shm_strategy_keyfile = "/tmp/shm_strategy_keyfile"
@@ -164,55 +153,12 @@ def get_shm_variable():
     shm_strategy_key = ctypes.CDLL(None).ftok(shm_strategy_keyfile.encode(), ord('R'))
     shm_persistence_size_key = ctypes.CDLL(None).ftok(shm_persistence_size_keyfile.encode(), ord('R'))
 
-    # it seems like time.sleep(1) is necessary
+    # it seems like time.sleep() is necessary
     time.sleep(0.5)
 
     shm_cost_model_flag = sysv_ipc.SharedMemory(shm_cost_model_flag_key, ctypes.sizeof(ctypes.c_uint16), 0 | 0o666)
     shm_strategy = sysv_ipc.SharedMemory(shm_strategy_key, ctypes.sizeof(ctypes.c_uint16), 0 | 0o666)
     shm_persistence_size = sysv_ipc.SharedMemory(shm_persistence_size_key, ctypes.sizeof(ctypes.c_uint64), 0 | 0o666)
-
-    return shm_cost_model_flag, shm_strategy, shm_persistence_size
-
-
-def detach_shm_variable(*args):
-    for v in args:
-        v.detach()
-    print(f'Detached shared memory variable from Ratchet')
-
-
-def cost_model(pt, end_time, current_time,
-               latency_ppl_suspend,
-               latency_ppl_resume,
-               latency_proc_suspend,
-               latency_proc_resume):
-    cost_redo = pt * end_time
-    cost_ppl = (1 - pt) * (latency_ppl_suspend + latency_ppl_resume) + (pt * current_time)
-    cost_proc = (1 - pt) * (latency_proc_suspend + latency_proc_resume) + pt * current_time
-    cost_list = [cost_redo, cost_proc, cost_ppl]
-
-    return np.where(cost_list == np.min(cost_list))[0]
-
-
-def func_persistence_size(x, a, b, c, d, e):
-    r = a * x[0] + b * x[1] + c * x[2] + d * x[3] + e
-    return r.ravel()
-
-
-def main():
-    # Set the output format
-    pd.set_option('display.float_format', '{:.1f}'.format)
-
-    # Get start time point
-    start_time = time.perf_counter()
-
-    # Get the execution command, termination window start and end
-    ratchet_cmd, ts, te, time_step, num_join, num_groupby, input_card = assemble_execution_cmd()
-
-    # Execute the query through subprocess
-    ratchet_proc = subprocess.Popen([ratchet_cmd], shell=True)
-
-    # Get shared memory for IPC
-    shm_cost_model_flag, shm_strategy, shm_persistence_size = get_shm_variable()
 
     while True:
         # Convert share memory to variables
@@ -226,45 +172,70 @@ def main():
 
         time.sleep(0.5)
 
-    # Get end time point
-    end_time = time.perf_counter()
-    # Get time duration
-    time_duration = end_time - start_time
+    # Get pipeline suspension time
+    ppl_suspension_time = time.perf_counter()
 
-    # Create an estimator for pipeline latency estimation
     ppl_estimator = PipelineLatencyEstimator(persistence_size, RAND_WRITE_SPEED, RAND_READ_SPEED)
     latency_ppl_suspend = ppl_estimator.suspend_latency_estimation()
     latency_ppl_resume = ppl_estimator.resume_latency_estimation()
 
+    # Load json file for estimation
+    query_json = PropertyUtils.load_property_file(properties_file=estimation_file)
+    query_executions = query_json["query_executions"]
+
     # Create an estimator for process latency estimation
-    num_join_array = None
-    input_cardinality_array = None
-    suspension_point_array = None
-    persistence_size_array = None
-    num_groupby_array = None
+    num_join_list = list()
+    num_groupby_list = list()
+    input_card_list = list()
+    suspension_point_list = list()
+    persist_size_list = list()
+
+    for qe in query_executions:
+        num_join_list.append(qe["num_join"])
+        num_groupby_list.append(qe["num_groupby"])
+        input_card_list.append(qe["input_cardinality"])
+        suspension_point_list.append(qe["suspension_point"])
+        persist_size_list.append(qe["persistence_size"])
 
     proc_estimator = ProcLatencyEstimator(RAND_WRITE_SPEED, RAND_READ_SPEED,
-                                          num_join_array, num_groupby_array,
-                                          input_cardinality_array, suspension_point_array,
-                                          persistence_size_array)
+                                          num_join_list, num_groupby_list,
+                                          input_card_list, suspension_point_list, persist_size_list)
     proc_estimator.fit_curve()
 
-    suspend_time = start_time
+    proc_suspension_time = ppl_suspension_time
     latency_proc_suspend = float('inf')
     latency_proc_resume = float('inf')
 
-    while suspend_time <= te:
-        suspend_time += time_step
-        latency_proc_suspend_est = proc_estimator.suspend_latency_estimation(num_join, input_card, suspend_time)
+    # Probe the "best" proc suspension time based on latency
+    while proc_suspension_time <= te:
+        proc_suspension_time += time_step
+        latency_proc_suspend_est = proc_estimator.suspend_latency_estimation(num_join, num_groupby,
+                                                                             input_card, proc_suspension_time)
         if latency_proc_suspend_est < latency_proc_suspend:
             latency_proc_suspend = latency_proc_suspend_est
-        latency_proc_resume_est = proc_estimator.resume_latency_estimation(num_join, input_card, suspend_time)
+
+        latency_proc_resume_est = proc_estimator.resume_latency_estimation(num_join, num_groupby,
+                                                                           input_card, proc_suspension_time)
         if latency_proc_resume_est < latency_proc_resume:
             latency_proc_resume = latency_proc_resume_est
 
-    strategy_id = cost_model(TERM_PROB, end_time, time_duration,
-                             latency_ppl_suspend, latency_ppl_resume,
-                             latency_proc_suspend, latency_proc_resume) + 1
+    ######################
+    # Cost Model Decision
+    ######################
+    cost_redo = TERM_PROB * ppl_suspension_time
+    if ppl_suspension_time + latency_ppl_suspend > ts:
+        prob_ppl_term = TERM_PROB
+    else:
+        prob_ppl_term = 0
+    cost_ppl = latency_ppl_suspend + latency_ppl_resume + (prob_ppl_term * ppl_suspension_time)
+
+    if proc_suspension_time + latency_proc_suspend > ts:
+        prob_proc_term = TERM_PROB
+    else:
+        prob_proc_term = 0
+    cost_proc = latency_proc_suspend + latency_proc_resume + (prob_proc_term * proc_suspension_time)
+    cost_list = [cost_redo, cost_proc, cost_ppl]
+    strategy_id = np.where(cost_list == np.min(cost_list))[0]
 
     # 1: Redo Strategy
     # 2: Process-level Strategy
@@ -286,7 +257,21 @@ def main():
     return_code = ratchet_proc.returncode
     print(f'[Python] Ratchet exited with return code: {return_code}')
 
-    detach_shm_variable(shm_cost_model_flag, shm_strategy, shm_persistence_size)
+    shm_cost_model_flag.detach()
+    shm_strategy.detach()
+    shm_persistence_size.detach()
+    print(f'Detached shared memory variable from Ratchet')
+
+
+def main():
+    # Set the output format
+    pd.set_option('display.float_format', '{:.1f}'.format)
+
+    # Demo for Estimating Latency of Process-level Strategy
+    demo_proc_latency_estimation()
+
+    # Demo for End-to-End Pipeline
+    demo_e2e()
 
 
 if __name__ == "__main__":
